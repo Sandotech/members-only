03a8dc0b1bf2f9e82f5b1a6c9680629032f05285
santodev <dseosantos76@gmail.com>
2023-09-17 14:48:28 -0400

    sign-in/out link in home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
+
+  devise_scope :user do
+    get 'sign_out', to: 'devise/sessions#destroy', as: 'sign_out_path'
+    get 'sign_in', to: 'devise/sessions#new', as: 'sign_in_path'
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'posts#index'
-  
+
   resources :posts, only: [:new, :create, :index]
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
